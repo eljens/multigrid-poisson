1afca3c78d065a45443ab8a9d419348cb1b5470a
s174515 <s174515@student.dtu.dk>
2022-11-24 15:47:36 +0100

    First convergence test with Neumann boundaries

--- openmp/convergence.sh
+++ openmp/convergence.sh
@@ -12,9 +12,9 @@ make
 FILENAME=vcycle.txt
 
 MAXITER=10
-LEVELS=2
+LEVELS=3
 
-for N in 5 9 17 33 65 129 257 513 1025
+for N in 9 17 33 65 129 257 513 1025
 do
     ./bin/driver -x $N -y $N -z $N -maxiter $MAXITER -levels $LEVELS -save 0 -length 5 -stats $FILENAME
     LEVELS=$(($LEVELS + 1))
