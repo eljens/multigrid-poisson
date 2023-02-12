181249dcc88e55f2378f7e6f0a3e2f9302251e05
Anton Rydahl <s174515@student.dtu.dk>
2023-02-12 14:32:14 +0200

    Compatibility with MI250X in clang++ and g++

--- openmp/paths/lumi/gcc.sh
+++ openmp/paths/lumi/gcc.sh
@@ -1,4 +1,8 @@
+export LUMI=1
+module load rocm
 export LD_LIBRARY_PATH=~/GCC/13/gcc-offload/install/lib64:$LD_LIBRARY_PATH
 export PATH=/pfs/lustrep2/users/rydahlan/GCC/13/gcc-offload/install/bin:$PATH
-echo $LD_LIBRARY_PATH
+export ROCR_VISIBLE_DEVICES=0
+export OMP_TARGET_OFFLOAD=MANDATORY
 echo $PATH
+echo $LD_LIBRARY_PATH
