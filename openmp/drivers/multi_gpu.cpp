ca4bdd36b2fa389a1768b1a3dc5deded959c415c
s174515 <s174515@student.dtu.dk>
2023-04-27 11:34:25 +0200

    First step towards multi-gpu version. Internal boundaries are not implementated but the PoissonSolver class handles an array of domains

---
+++ openmp/drivers/multi_gpu.cpp
@@ -0,0 +1,68 @@
+#include "parser.h"
+#include "domainsettings.h"
+#include "libpoisson.h"
+#include "definitions.h"
+#include "devicearray.h"
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using Poisson::PoissonSolver;
+using Poisson::Settings;
+using Poisson::parser;
+using Poisson::FullWeighting;
+using Poisson::Injection;
+using Poisson::GaussSeidel;
+using Poisson::TrilinearInterpolation;
+using Poisson::Jacobi;
+using Poisson::DeviceArray;
+using Poisson::int_t;
+using Poisson::uint_t;
+
+int main(int argc, char * argv[]){
+    bool is_dirichlet = false;
+    uint_t num_devices = omp_get_num_devices();
+    Settings settings = parser(argc,argv);
+    PoissonSolver<double_t,Injection,TrilinearInterpolation,Jacobi> solver(num_devices,settings,is_dirichlet);
+    solver.init();
+    solver.verbose(true);
+    solver.to_device();
+
+    solver.solve("fcycle",4);
+    cout << "It took " << solver.solve_time() << " seconds to run ";
+    cout << solver.solve_iterations() << " Fcycles"<<endl;
+
+    solver.to_host();
+    
+    for (int_t gpuid = 0; gpuid < num_devices; gpuid++){
+        if (settings.print_result){
+            solver.save_all(gpuid,
+                "results/u_"+std::to_string(gpuid)+".vtk",
+                "results/f_"+std::to_string(gpuid)+".vtk",
+                "results/r_"+std::to_string(gpuid)+".vtk");
+        }
+
+        const DeviceArray<double_t> & u = solver.get_solution(gpuid);
+
+        double_t err = 0.0;
+
+        #pragma omp parallel for collapse(3) reduction(max:err)
+        for (int_t i = 0;i<u.shape[0];i++){
+            for (int_t j = 0;j<u.shape[1];j++){
+                for (int_t k = 0;k<u.shape[2];k++){
+                    double_t u_true = 
+                        Poisson::ufun(settings.origin[0]+settings.lengthx*gpuid+((double_t) i)*settings.h,
+                        settings.origin[1]+((double_t) j)*settings.h,
+                        settings.origin[2]+((double_t) k)*settings.h);
+                    double tmp = abs(u.at[u.idx(i,j,k)]-u_true);
+                    err = std::max(err,tmp);
+                }
+            }
+        }
+
+        cout << "Maximal error: " << setw(8) << err << endl;
+    }
+
+    return EXIT_SUCCESS;
+}
--- openmp/include/libpoisson.h
+++ openmp/include/libpoisson.h
@@ -25,7 +25,8 @@ namespace Poisson{
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     class PoissonSolver {
         private:
-            Domain<T> ** domains;
+            uint_t num_devices;
+            Domain<T> *** domains;
             Settings settings;
             Grid grid;
             R<T> restriction_type;
@@ -38,8 +39,8 @@ namespace Poisson{
             double_t wtime = 0.0;
             void alloc();
         public:
-            PoissonSolver(Settings & _settings,bool _is_dirichlet);
-            PoissonSolver(Settings & _settings);
+            PoissonSolver(uint_t ndev, Settings & _settings,bool _is_dirichlet);
+            PoissonSolver(uint_t ndev, Settings & _settings);
             ~PoissonSolver();
             void init();
             void init_zero();
@@ -47,30 +48,28 @@ namespace Poisson{
             void to_host();
             void verbose(bool onoff);
             void solve(const string solver="Vcycle",const int_t nsmooth=10,T omega=-1.0,string ofile="",double maxtime = 20*60);
-            void save(string file_name="u.vtk");
-            void save_all(string ufile_name="u.vtk",string ffile_name="f.vtk",string rfile_name="r.vtk");
+            void save(uint_t gpuid,string file_name="u.vtk");
+            void save_all(uint_t gpuid,string ufile_name="u.vtk",string ffile_name="f.vtk",string rfile_name="r.vtk");
             T relative_residual();
             T solve_time();
             T solve_iterations();
-            DeviceArray<T> & get_rhs();
-            const DeviceArray<T> & get_solution();
-            const DeviceArray<T> & get_residual();
-            Boundary<T> & get_top_bc();
-            Boundary<T> & get_bottom_bc();
+            DeviceArray<T> & get_rhs(uint_t gpuid);
+            const DeviceArray<T> & get_solution(uint_t gpuid);
+            const DeviceArray<T> & get_residual(uint_t gpuid);
+            Boundary<T> & get_top_bc(uint_t gpuid);
+            Boundary<T> & get_bottom_bc(uint_t gpuid);
     };
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    PoissonSolver<T,R,P,S>::PoissonSolver(Settings & _settings, bool _is_dirichlet) : 
-        settings(_settings), 
-        grid(_settings,_settings.levels), is_dirichlet(_is_dirichlet)
+    PoissonSolver<T,R,P,S>::PoissonSolver(uint_t ndev, Settings & _settings, bool _is_dirichlet) : 
+        num_devices(ndev), settings(_settings), grid(_settings,_settings.levels), is_dirichlet(_is_dirichlet)
     {
         this->alloc();
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    PoissonSolver<T,R,P,S>::PoissonSolver(Settings & _settings) : 
-        settings(_settings),
-        grid(_settings,_settings.levels), is_dirichlet(false)
+    PoissonSolver<T,R,P,S>::PoissonSolver(uint_t ndev, Settings & _settings) : 
+        num_devices(ndev), settings(_settings), grid(_settings,_settings.levels), is_dirichlet(false)
     {
         this->alloc();
     }
@@ -78,8 +77,11 @@ namespace Poisson{
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     PoissonSolver<T,R,P,S>::~PoissonSolver()
     {
-        for (uint_t l = 0;l<settings.levels;l++){
-            delete domains[l];
+        for (uint_t gpuid = 0;gpuid<num_devices;gpuid++){
+            for (uint_t l = 0;l<settings.levels;l++){
+                delete domains[gpuid][l];
+            }
+            delete domains[gpuid];
         }
         delete[] domains;
     }
@@ -87,21 +89,27 @@ namespace Poisson{
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     void PoissonSolver<T,R,P,S>::alloc()
     {
-        domains = new Domain<T>*[settings.levels];
-        for (uint_t l = 0;l<settings.levels;l++){
-            domains[l] = new Domain<T>(grid.domainsettings[l],is_dirichlet,relaxation.requires_duplicate_solution());
+        domains = new Domain<T>**[num_devices];
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            domains[gpuid] = new Domain<T>*[settings.levels];
+            for (uint_t l = 0;l<settings.levels;l++){
+                domains[gpuid][l] = new Domain<T>(grid.domainsettings[l],is_dirichlet,relaxation.requires_duplicate_solution());
+                grid.domainsettings[l].origin[0] += grid.domainsettings[l].lengthx;
+            }
         }
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     void PoissonSolver<T,R,P,S>::init()
-    {
-        for (uint_t l = 0;l<settings.levels;l++){
-            if (l==0){
-                domains[l]->init(&ufun,&ffun,&dudxfun,&dudyfun);
-            }
-            else {
-                domains[l]->init_zero();
+    {   
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            for (uint_t l = 0;l<settings.levels;l++){
+                if (l==0){
+                    domains[gpuid][l]->init(&ufun,&ffun,&dudxfun,&dudyfun);
+                }
+                else {
+                    domains[gpuid][l]->init_zero();
+                }
             }
         }
     }
@@ -109,23 +117,29 @@ namespace Poisson{
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     void PoissonSolver<T,R,P,S>::init_zero()
     {
-        for (uint_t l = 0;l<settings.levels;l++){
-            domains[l]->init_zero();
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            for (uint_t l = 0;l<settings.levels;l++){
+                domains[gpuid][l]->init_zero();
+            }
         }
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     void PoissonSolver<T,R,P,S>::to_device()
     {
-        for (uint_t l = 0;l<settings.levels;l++){
-            domains[l]->to_device();
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            for (uint_t l = 0;l<settings.levels;l++){
+                domains[gpuid][l]->to_device();
+            }
         }
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
     void PoissonSolver<T,R,P,S>::to_host()
     {
-        domains[0]->to_host();
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            domains[gpuid][0]->to_host();
+        }
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
@@ -148,9 +162,14 @@ namespace Poisson{
         if (omega < 0) omega = relaxation.default_omega();
 
         iter = 0;
-        double_t fnorm = domains[0]->f->infinity_norm();
-        residual<T>(*domains[0]);
-        rel_res = domains[0]->r->infinity_norm() / fnorm;
+        double_t fnorm = 0.0;
+        double_t rnorm = 0.0;
+        for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+            fnorm += domains[gpuid][0]->f->infinity_norm();
+            residual<T>(*domains[gpuid][0]);
+            rnorm += domains[gpuid][0]->r->infinity_norm();
+        }
+        rel_res = rnorm / fnorm;
 
         if (is_verbose){
             cout << setw(4) << 0 << ": Initial residual: " << setw(8) << rel_res << endl;
@@ -161,16 +180,28 @@ namespace Poisson{
         }
         for(iter = 0;iter<settings.maxiter;iter++){
             if (use_vcycle){
-                Vcycle<T>(this->domains,this->restriction_type,this->prolongation,this->relaxation,omega,0,settings.levels,nsmooth);
+                for (uint_t gpuid = 0; gpuid < num_devices; gpuid++){
+                    Vcycle<T>(this->domains[gpuid],this->restriction_type,this->prolongation,this->relaxation,omega,0,settings.levels,nsmooth);
+                }
             }
             else if (use_fcycle){
-                Fcycle<T>(this->domains,this->restriction_type,this->prolongation,this->relaxation,omega,0,settings.levels,nsmooth);
+                for (uint_t gpuid = 0; gpuid < num_devices; gpuid++){
+                    Fcycle<T>(this->domains[gpuid],this->restriction_type,this->prolongation,this->relaxation,omega,0,settings.levels,nsmooth);
+                }
             }
             else {
-                relaxation.relax(*domains[0],omega);
+                for (uint_t gpuid = 0; gpuid<num_devices;gpuid++){
+                    relaxation.relax(*domains[gpuid][0],omega);
+                }
             }
-            residual<T>(*domains[0]);
-            T norm = domains[0]->r->infinity_norm() / fnorm;
+            for (uint_t gpuid = 0; gpuid < num_devices; gpuid++){
+                residual<T>(*domains[gpuid][0]);
+            }
+            T norm = 0.0;
+            for (uint_t gpuid = 0; gpuid < num_devices; gpuid++){
+                norm += domains[gpuid][0]->r->infinity_norm();
+            }
+            norm /= fnorm;
             if (ofile.compare("")!=0){
                 out << setw(12) << omp_get_wtime()-wtime;
                 out << setw(12) << this->relative_residual();
@@ -199,15 +230,15 @@ namespace Poisson{
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    void PoissonSolver<T,R,P,S>::save(string file_name)
+    void PoissonSolver<T,R,P,S>::save(uint_t gpuid, string file_name)
     {
-        domains[0]->save(file_name);
+        domains[gpuid][0]->save(file_name);
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    void PoissonSolver<T,R,P,S>::save_all(string ufile_name,string ffile_name,string rfile_name)
+    void PoissonSolver<T,R,P,S>::save_all(uint_t gpuid, string ufile_name,string ffile_name,string rfile_name)
     {
-        domains[0]->save_all(ufile_name,ffile_name,rfile_name);
+        domains[gpuid][0]->save_all(ufile_name,ffile_name,rfile_name);
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
@@ -229,28 +260,28 @@ namespace Poisson{
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    DeviceArray<T> & PoissonSolver<T,R,P,S>::get_rhs(){
-        return *(domains[0]->f);
+    DeviceArray<T> & PoissonSolver<T,R,P,S>::get_rhs(uint_t gpuid){
+        return *(domains[gpuid][0]->f);
     }
 
    template <class T,template<class> class R,template<class> class P,template<class> class S>
-    const DeviceArray<T> & PoissonSolver<T,R,P,S>::get_solution(){
-        return *(domains[0]->u);
+    const DeviceArray<T> & PoissonSolver<T,R,P,S>::get_solution(uint_t gpuid){
+        return *(domains[gpuid][0]->u);
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    const DeviceArray<T> & PoissonSolver<T,R,P,S>::get_residual(){
-        return *(domains[0]->r);
+    const DeviceArray<T> & PoissonSolver<T,R,P,S>::get_residual(uint_t gpuid){
+        return *(domains[gpuid][0]->r);
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    Boundary<T> & PoissonSolver<T,R,P,S>::get_top_bc(){
-        return *(domains[0]->top);
+    Boundary<T> & PoissonSolver<T,R,P,S>::get_top_bc(uint_t gpuid){
+        return *(domains[gpuid][0]->top);
     }
 
     template <class T,template<class> class R,template<class> class P,template<class> class S>
-    Boundary<T> & PoissonSolver<T,R,P,S>::get_bottom_bc(){
-        return *(domains[0]->bottom);
+    Boundary<T> & PoissonSolver<T,R,P,S>::get_bottom_bc(uint_t gpuid){
+        return *(domains[gpuid][0]->bottom);
     }
 }
 #endif
